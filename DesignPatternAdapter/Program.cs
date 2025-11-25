001a44dd888d8973eea34a22ce4d164049761ae4
Brandner Christoph <bn@htlwy.at>
2025-11-25 21:01:27 +0100

    adapter design pattern added

---
+++ DesignPatternAdapter/Program.cs
@@ -0,0 +1,12 @@
+﻿// Client
+
+using DesignPatternAdapter;
+
+Console.WriteLine("Adapter Example");
+float inputVal = 3.1415f;
+ICalculator calculator = new CalculatorMultiplication();
+
+//Console.WriteLine(calculator.CalculateValue(inputVal).ToString());  // Fehler, da kein korrekter Typ übergeben wird (int / float)
+
+Target t = new Adapter(calculator);
+Console.WriteLine(t.CalculateValue(inputVal));
---
+++ DesignPatternAdapter/SimpleAdapterExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternAdapter
+{
+    // Target
+    interface Target
+    {
+        int CalculateValue(float value);
+    }
+
+    // Adapter
+    class Adapter : Target
+    {
+        ICalculator adaptee;    // Instanz des Adaptee
+
+        public Adapter(ICalculator calc)
+        {
+            this.adaptee = calc;
+        }
+
+        public int CalculateValue(float value)
+        {
+            int castedValue = (int)value;
+            return this.adaptee.CalculateValue(castedValue);
+        }
+    }
+
+     
+
+    // Adaptee Interface
+    interface ICalculator
+    {
+        int CalculateValue(int value);
+    }
+
+    class CalculatorMultiplication : ICalculator
+    {
+        public int CalculateValue(int value) { 
+            return value * value; 
+        }
+    }
+}
